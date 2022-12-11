e554eed3f80caa0ba67103328030e6eb6c40342b
hsohliyt105 <62872159+hsohliyt105@users.noreply.github.com>
2022-12-11 19:48:17 +0900

    Add files via upload

--- helper.py
+++ helper.py
@@ -9,9 +9,9 @@ command_help = {
 
 command_option = { 
     'help': '`command name` Example: `!help four`',
-    'four': '`option=<option>` Option list (first option is the default): `transparent(t)=yes(y)/no(n)` (not supported in gif), `theme=dark/light`, `duration(d)=0.5/<delay per frame in seconds>` `background(b)=#36393f/<hex colour code>` Example: `!fumen v115@HhwhglQpAtwwg0Q4C8JewhglQpAtwwg0Q4A8LeAgH duration=1 t=n b=#FFFFFF' 
+    'four': '`option=<option>` \nOption list (first option is the default): \n`transparent(t)=yes(y)/no(n)` (not supported in gif), \n`theme=dark/light`, \n`duration(d)=0.5/<delay per frame in seconds>` \n`background(b)=#36393f/<hex colour code>` \nExample: `!fumen v115@HhwhglQpAtwwg0Q4C8JewhglQpAtwwg0Q4A8LeAgH duration=1 t=n b=#FFFFFF`' 
     }
 
-version = "1.0.0"
+version = "0.0.4"
 
 presence_time = 60
