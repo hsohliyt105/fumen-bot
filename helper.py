bb1cebd681a79ebb725192a27ba80ac7fa976574
hsohliyt105 <62872159+hsohliyt105@users.noreply.github.com>
2023-01-10 01:06:25 +0800

    0.0.6

--- helper.py
+++ helper.py
@@ -12,6 +12,6 @@ command_option = {
     'four': '`option=<option>` \nOption list (first option is the default): \n`transparent(t)=yes(y)/no(n)` (not supported in gif), \n`theme=dark/light`, \n`duration(d)=0.5/<delay per frame in seconds>` \n`background(b)=#36393f/<hex colour code>` \nExample: `!four v115@HhwhglQpAtwwg0Q4C8JewhglQpAtwwg0Q4A8LeAgH duration=1 t=n b=#FFFFFF`' 
     }
 
-version = "0.0.5"
+version = "0.0.6"
 
 presence_time = 60
